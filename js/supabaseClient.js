8ebb86e96ebd83af1acd43d7672aa0b9293c5646
Castillo <castillocris14@gmail.com>
2026-03-02 17:06:58 -0300

    Cambios para subir a pages

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,18 +1,4 @@
-window.sb = window.supabase.createClient(
-  "https://bqkxxonbsezfesozeahy.supabase.co",
-  "sb_publishable_OPjczO2zdR00Mc6dF7xRnQ_CBCyCtit"
-);
+const SUPABASE_URL = "https://bqkxxonbsezfesozeahy.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_OPjczO2zdR00Mc6dF7xRnQ_CBCyCtit";
 
-async function cargarProductos() {
-  const { data, error } = await supabase
-    .from('productos')
-    .select('*')
-    .eq('activo', true);
-
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  mostrarProductos(data);
-}
+window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
